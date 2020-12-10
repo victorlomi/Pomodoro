2f817d843fc2c50d8ef9d67ac4b3137527a38e90
Rovine1999 <rovinewanjala99@gmail.com>
2020-12-10 10:47:17 +0300

    add  settings forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
-class LoginForm(FlaskForm):
+class SettingsForm(FlaskForm):
     type_of_break = StringField('Type of break', validators=[DataRequired()])
     time = IntegerField('How long you want to spend working?', validators=[DataRequired()]) 
     submit = SubmitField('Submit')
