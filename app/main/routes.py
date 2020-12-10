ca148d3b683f6c604bfb00af9bd5ed935411fc8d
Rovine1999 <rovinewanjala99@gmail.com>
2020-12-10 11:56:24 +0300

    add route for timer and import url_for and redirect

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.main import bp
 from app.main.forms import SettingsForm
 
@@ -9,4 +9,11 @@ def index():
 @bp.route('/settings', methods=["GET", "POST"])
 def settings():
     form = SettingsForm()
+    if form.validate_on_submit():
+        return redirect(url_for('main.timer')) 
     return render_template('settings.html', form=form)
+
+
+@bp.route('/timer')
+def timer():
+    return 'timer'
