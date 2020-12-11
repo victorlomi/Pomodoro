4d968a61dd688983c95fabe63e72e80c6b225414
victorlomi <victorlominyo@gmail.com>
2020-12-11 03:06:22 +0300

    add number range validator for min and max of break time and work time

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, NumberRange
 from app.models import User
 
 class SettingsForm(FlaskForm):
     type_of_break = StringField('What do you want to do on your break?', validators=[DataRequired()])
-    time = IntegerField('How long you want to spend working?', validators=[DataRequired()])
-    break_time = IntegerField('How long you want to break for?', validators=[DataRequired()])
+    time = IntegerField('How long you want to spend working?', validators=[DataRequired(), NumberRange(min=1, max=60)])
+    break_time = IntegerField('How long you want to break for?', validators=[DataRequired(), NumberRange(min=5, max=10)])
     submit = SubmitField('Submit')
